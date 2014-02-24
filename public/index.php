b53abe73cde443f24b06acc5dc9462acb870edeb
Alan Holding <alanholding@me.com>
2014-02-24 14:28:29 +0000

    Starts to map routes to OOP classes and methods

--- public/index.php
+++ public/index.php
@@ -1,12 +1,16 @@
 <?php
 
+class API {
+
+	function hello() {
+    echo 'Hi there! This is the MDDA WSN API v1.0. Glad to be of service.';
+	}
+
+}
+
 $f3 = require('../lib/base.php');
 
-$f3->route('GET /',
-    function() {
-        echo 'Hi there! This is the MDDA WSN API v1.0. Glad to be of service.';
-    }
-);
+$f3->route('GET /', 'API->hello');
 
 $f3->run();
 
