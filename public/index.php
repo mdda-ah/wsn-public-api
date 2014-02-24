9d543e421893abb9fbeae34cfec274074ef14496
Alan Holding <alanholding@me.com>
2014-02-24 14:32:42 +0000

    Adds .htaccess and util/ping route

--- public/index.php
+++ public/index.php
@@ -6,11 +6,17 @@ class API {
     echo 'Hi there! This is the MDDA WSN API v1.0. Glad to be of service.';
 	}
 
+	function ping() {
+    echo 'OK';
+	}
+
 }
 
 $f3 = require('../lib/base.php');
 
 $f3->route('GET /', 'API->hello');
+
+$f3->route('GET /util/ping', 'API->ping');
 
 $f3->run();
 
