318da5f8c53fcb5d9428197e3d988cb4f6dd1806
Alan Holding <alanholding@me.com>
2014-02-26 14:15:44 +0000

    Adds first version of working data/all method

--- classes/data.php
+++ classes/data.php
@@ -7,8 +7,23 @@ class Data {
 		echo '/units/data/' . $id;
 	}
 
-	function all() {
-		echo '/units/data/all';
+	function all($f3) {
+		$db = new DB\SQL(
+			'mysql:host=localhost;port=3306;dbname=sensors',
+			'sensors',
+			'sensors23'
+		);
+
+		$f3->set(
+			'result',
+			$db->exec(
+				'SELECT * FROM readings LIMIT 0,?',
+				25
+			)
+		);
+
+  	header('Content-Type: application/json');
+		echo json_encode($f3->get('result'));
 	}
 
 }
