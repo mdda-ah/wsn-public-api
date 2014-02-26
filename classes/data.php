54b3945703faa5cdde991515f2dec913c1628984
Alan Holding <alanholding@me.com>
2014-02-26 15:08:03 +0000

    Adds database query limit parameters with validation to data/all method

--- classes/data.php
+++ classes/data.php
@@ -14,16 +14,31 @@ class Data {
 			'sensors23'
 		);
 
+		//	Get the limit parameter from the request, if present. If NAN or less than 1 then use the default limit.
+		$f3->set(
+			'database_query_limit',
+			(
+				//	Check that GET.limit is a number
+				preg_match("/[0-9]/us", $f3->get('GET.limit'))
+				//	Check that GET.limit is greater than 0 and ensure does not exceed maximum query limit
+				? ($f3->get('GET.limit') > 0 ? min((int)$f3->get('GET.limit'),(int)$f3->get('db_defaults["query_limit_max"]')) : (int)$f3->get('db_defaults["query_limit"]'))
+				//	Use the default query limit
+				: (int)$f3->get('db_defaults["query_limit"]')
+			)
+		);
+
 		$f3->set(
 			'result',
 			$db->exec(
 				'SELECT * FROM readings LIMIT 0,?',
-				25
+				$f3->get('database_query_limit')
 			)
 		);
 
-  	header('Content-Type: application/json');
-		echo json_encode($f3->get('result'));
+		$response_data = ($f3->get('result') ? $f3->get('result') : "No results");
+
+	 	header('Content-Type: application/json');
+		echo json_encode($response_data);
 	}
 
 }
