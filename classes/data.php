bcdfebad9c08896f4f2d62dec15797f66cfd4989
Alan Holding <alanholding@me.com>
2014-02-26 15:23:04 +0000

    Adds Helper class and moves repeatable functions from data/all into it

--- classes/data.php
+++ classes/data.php
@@ -8,24 +8,15 @@ class Data {
 	}
 
 	function all($f3) {
+		$helper = new Helper();
+
 		$db = new DB\SQL(
 			'mysql:host=localhost;port=3306;dbname=sensors',
 			'sensors',
 			'sensors23'
 		);
 
-		//	Get the limit parameter from the request, if present. If NAN or less than 1 then use the default limit.
-		$f3->set(
-			'database_query_limit',
-			(
-				//	Check that GET.limit is a number
-				preg_match("/[0-9]/us", $f3->get('GET.limit'))
-				//	Check that GET.limit is greater than 0 and ensure does not exceed maximum query limit
-				? ($f3->get('GET.limit') > 0 ? min((int)$f3->get('GET.limit'),(int)$f3->get('db_defaults["query_limit_max"]')) : (int)$f3->get('db_defaults["query_limit"]'))
-				//	Use the default query limit
-				: (int)$f3->get('db_defaults["query_limit"]')
-			)
-		);
+		$helper->set_database_query_limit($f3);
 
 		$f3->set(
 			'result',
@@ -35,10 +26,12 @@ class Data {
 			)
 		);
 
-		$response_data = ($f3->get('result') ? $f3->get('result') : "No results");
+		$f3->set(
+			'response_data',
+			($f3->get('result') ? $f3->get('result') : "No results")
+		);
 
-	 	header('Content-Type: application/json');
-		echo json_encode($response_data);
+		$helper->send_response($f3);
 	}
 
 }
---
+++ classes/helper.php
@@ -0,0 +1,27 @@
+<?php
+
+class Helper {
+
+	//	Get the limit parameter from the request, if present. If NAN or less than 1 then use the default limit.
+	function set_database_query_limit($f3) {
+		$f3->set(
+			'database_query_limit',
+			(
+				//	Check that GET.limit is a number
+				preg_match("/[0-9]/us", $f3->get('GET.limit'))
+				//	Check that GET.limit is greater than 0 and ensure does not exceed maximum query limit
+				? ($f3->get('GET.limit') > 0 ? min((int)$f3->get('GET.limit'),(int)$f3->get('db_defaults["query_limit_max"]')) : (int)$f3->get('db_defaults["query_limit"]'))
+				//	Use the default query limit
+				: (int)$f3->get('db_defaults["query_limit"]')
+			)
+		);
+	}
+
+	function send_response($f3) {
+	 	header('Content-Type: application/json');
+		echo json_encode($f3->get('response_data'));
+	}
+
+}
+
+?>
