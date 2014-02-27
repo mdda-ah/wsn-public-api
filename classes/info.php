ad958219ea370d4bc34ebdd329060d2e59a7f05f
Alan Holding <alanholding@me.com>
2014-02-27 14:08:35 +0000

    Adds first version of working info/get method to API

--- classes/info.php
+++ classes/info.php
@@ -3,8 +3,29 @@
 class Info {
 
 	function get($f3) {
-		$id = $f3->get('PARAMS.id');
-		echo '/units/info/' . $id;
+		global $db;
+
+		$helper = new Helper();
+
+		$helper->set_database_query_limit($f3);
+
+		$f3->set(
+			'result',
+			$db->exec(
+				$f3->get('dbq.info_get'),
+				array (
+					':device_id' => $f3->get('PARAMS.id'),
+					':database_query_limit' => $f3->get('database_query_limit')
+				)
+			)
+		);
+
+		$f3->set(
+			'response_data',
+			($f3->get('result') ? $f3->get('result') : "No results")
+		);
+
+		$helper->send_response($f3);
 	}
 
 	function all() {
