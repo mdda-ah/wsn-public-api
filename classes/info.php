161af2e4addfa6b47e6dbdf2a6797982dafa6883
Alan Holding <alanholding@me.com>
2014-02-27 14:34:16 +0000

    Adds first version of working info/all method to API

--- classes/info.php
+++ classes/info.php
@@ -28,8 +28,27 @@ class Info {
 		$helper->send_response($f3);
 	}
 
-	function all() {
-		echo '/units/info/all';
+	function all($f3) {
+		global $db;
+
+		$helper = new Helper();
+
+		$helper->set_database_query_limit($f3);
+
+		$f3->set(
+			'result',
+			$db->exec(
+				$f3->get('dbq.info_all'),
+				$f3->get('database_query_limit')
+			)
+		);
+
+		$f3->set(
+			'response_data',
+			($f3->get('result') ? $f3->get('result') : "No results")
+		);
+
+		$helper->send_response($f3);
 	}
 
 	function sensor_types($f3) {
