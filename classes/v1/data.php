3d790255cde54f821932ec7e338e1839a1fe8382
Alan Holding <alanholding@me.com>
2014-03-05 10:46:26 +0000

    Changes fields returned by data methods

--- classes/v1/data.php
+++ classes/v1/data.php
@@ -16,7 +16,7 @@ class Data {
 			$f3->set(
 				'result',
 				$db->exec(
-					'select coordcounter, datetime, device_id, reading, nodecounter, sensor_id from readings where device_id=:device_id and sensor_id in (select sensor_type_id from sensors where sensors.available=1 and sensors.device_id=readings.device_id) order by datetime desc limit 0,:database_query_limit',
+					'select datetime, device_id, sensor_id, value, bounds_flag, latitude, longitude, elevation_above_ground, nodecounter, coordcounter from readings where device_id=:device_id and sensor_id in (select sensor_type_id from sensors where sensors.available=1 and sensors.device_id=readings.device_id) order by datetime desc limit 0,:database_query_limit',
 					array (
 						':device_id' => $f3->get('PARAMS.id'),
 						':database_query_limit' => $f3->get('database_query_limit')
@@ -27,7 +27,7 @@ class Data {
 			$f3->set(
 				'result',
 				$db->exec(
-					'select coordcounter, datetime, device_id, reading, nodecounter, sensor_id from readings where device_id=:device_id and sensor_id=:sensor_type_id and sensor_id in (select sensor_type_id from sensors where sensors.available=1 and sensors.device_id=readings.device_id) order by datetime desc limit 0,:database_query_limit',
+					'select datetime, device_id, sensor_id, value, bounds_flag, latitude, longitude, elevation_above_ground, nodecounter, coordcounter from readings where device_id=:device_id and sensor_id=:sensor_type_id and sensor_id in (select sensor_type_id from sensors where sensors.available=1 and sensors.device_id=readings.device_id) order by datetime desc limit 0,:database_query_limit',
 					array (
 						':device_id' => $f3->get('PARAMS.id'),
 						':database_query_limit' => $f3->get('database_query_limit'),
@@ -58,7 +58,7 @@ class Data {
 			$f3->set(
 				'result',
 				$db->exec(
-					'select device_id, sensor_id, datetime, reading, nodecounter, coordcounter from readings where sensor_id in (select sensor_type_id from sensors where sensors.available=1 and sensors.device_id=readings.device_id) order by datetime desc limit 0,?',
+					'select datetime, device_id, sensor_id, value, bounds_flag, latitude, longitude, elevation_above_ground, nodecounter, coordcounter from readings where sensor_id in (select sensor_type_id from sensors where sensors.available=1 and sensors.device_id=readings.device_id) order by datetime desc limit 0,?',
 					$f3->get('database_query_limit')
 				)
 			);
@@ -66,7 +66,7 @@ class Data {
 			$f3->set(
 				'result',
 				$db->exec(
-					'select device_id, sensor_id, datetime, reading, nodecounter, coordcounter from readings where sensor_id=:sensor_type_id and sensor_id in (select sensor_type_id from sensors where sensors.available=1 and sensors.device_id=readings.device_id) order by datetime desc limit 0,:database_query_limit',
+					'select datetime, device_id, sensor_id, value, bounds_flag, latitude, longitude, elevation_above_ground, nodecounter, coordcounter from readings where sensor_id=:sensor_type_id and sensor_id in (select sensor_type_id from sensors where sensors.available=1 and sensors.device_id=readings.device_id) order by datetime desc limit 0,:database_query_limit',
 					array (
 						':database_query_limit' => $f3->get('database_query_limit'),
 						':sensor_type_id' => $f3->get('sensor_type_id')
@@ -93,7 +93,7 @@ class Data {
 		$f3->set(
 			'result',
 			$db->exec(
-				'select coordcounter, datetime, device_id, reading, nodecounter, sensor_id from readings where readings.id in (select * from (select max(readings.id) from readings group by readings.device_id, readings.sensor_id) as subquery) and sensor_id in (select sensor_type_id from sensors where sensors.available=1 and sensors.device_id=readings.device_id) order by device_id asc, datetime desc'
+				'select datetime, device_id, sensor_id, value, bounds_flag, latitude, longitude, elevation_above_ground, nodecounter, coordcounter from readings where readings.id in (select * from (select max(readings.id) from readings group by readings.device_id, readings.sensor_id) as subquery) and sensor_id in (select sensor_type_id from sensors where sensors.available=1 and sensors.device_id=readings.device_id) order by device_id asc, datetime desc'
 			)
 		);
 
