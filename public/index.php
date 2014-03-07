5f2eaa99f1c8ce23ce31c9b72a11075c18d65a64
Alan Holding <alanholding@me.com>
2014-03-07 13:53:19 +0000

    Moves database connection information to its own configuration file and moves that file out of the repo

--- public/index.php
+++ public/index.php
@@ -6,25 +6,22 @@ Include Fat Free Framework
 $f3 = require('../lib/base.php');
 
 /*
-Include application classes
-*/
-$f3->set('AUTOLOAD', '../classes/');
-
-/*
 Include configuration files
 Setup
 Routes and maps
+Database
 */
 $f3->config('../config/setup.cfg');
 $f3->config('../config/routesmaps.cfg');
+$f3->config('../config/db.cfg');
 
 /*
 Create the database object and connection
 */
 $db = new DB\SQL(
-	$f3->get('dbc.host'),
-	$f3->get('dbc.user'),
-	$f3->get('dbc.pass')
+	$f3->get('db.host'),
+	$f3->get('db.user'),
+	$f3->get('db.pass')
 );
 
 /*
