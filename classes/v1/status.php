5b5e519fdd75c99b50b52785c2fbe7af722a101a
Alan Holding <alanholding@me.com>
2014-03-12 17:00:22 +0000

    Adds status/battery method

---
+++ classes/v1/status.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace v1;
+
+class Status extends Controller {
+
+	function battery($f3) {
+		$this->check_database_is_up($f3);
+
+		$f3->set(
+			'result',
+			$this->db->exec(
+				'select device_id, datetime, value as battery_charge from readings where readings.id in (select * from (select max(readings.id) from readings group by readings.device_id, readings.sensor_id) as subquery) and sensor_id=999 and sensor_id in (select sensor_type_id from sensors where sensors.available=1 and sensors.device_id=readings.device_id) order by device_id asc, datetime desc'
+			)
+		);
+
+		if ($f3->get('result')) {
+
+		}
+
+		if ($f3->get('result')) {
+			$f3->mset(array(
+	    	'wsn.message'	=>	$f3->get('message_ok'),
+				'wsn.data'		=>	$f3->get('result')
+	    ));
+		} else {
+			$f3->mset(array(
+	    	'wsn.message'	=>	$f3->get('message_no_results'),
+				'wsn.data'		=>	null
+			));
+		}
+
+	}
+
+}
+
+?>
