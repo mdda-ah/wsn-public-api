af4bad4250a02ea3079d026219c53cdcfcec1e48
Alan Holding <alanholding@me.com>
2014-03-13 16:08:09 +0000

    Adds location name to status/battery method

--- classes/v1/status.php
+++ classes/v1/status.php
@@ -10,7 +10,7 @@ class Status extends Controller {
 		$f3->set(
 			'result',
 			$this->db->exec(
-				'select device_id, datetime, value as battery_charge from readings where readings.id in (select * from (select max(readings.id) from readings group by readings.device_id, readings.sensor_id) as subquery) and sensor_id=999 and sensor_id in (select sensor_type_id from sensors where sensors.available=1 and sensors.device_id=readings.device_id) order by device_id asc, datetime desc'
+				'select readings.device_id, devices.location_name, readings.datetime, readings.value as battery_charge from readings, devices where readings.id in (select * from (select max(readings.id) from readings group by readings.device_id, readings.sensor_id) as subquery) and sensor_id=999 and sensor_id in (select sensor_type_id from sensors where sensors.available=1 and sensors.device_id=readings.device_id) and readings.device_id=devices.device_id order by device_id asc, datetime desc'
 			)
 		);
 
