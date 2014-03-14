e21b2d49788169935246a417ff486c696a7c3a02
Alan Holding <alanholding@me.com>
2014-03-14 17:13:09 +0000

    Moves Fat Free Framework debug flag out of the main configuration file into a development only configuration file

--- public/index.php
+++ public/index.php
@@ -10,10 +10,16 @@ Include configuration files
 Setup
 Routes and maps
 Database
+Debug flag - on development copy only
 */
 $f3->config('../config/setup.cfg');
 $f3->config('../config/routesmaps.cfg');
 $f3->config('../config/db.cfg');
+try {
+	$f3->config('../config/debug.cfg');
+} catch (\ErrorException $e) {
+	//	This is my care face
+}
 
 /*
 Run Fat Free Framework
