d86225ea41e68b898e85db2fb3897afe9d2af3a8
MDDA Tech Team <tech+webdev@manchesterdda.com>
2014-05-13 11:07:13 +0100

    Changes the welcome message

--- dict/en.php
+++ dict/en.php
@@ -6,5 +6,5 @@ return array(
 	'message_database_is_down'	=>	'Oops. The database server appears to be down. Panic!',
 	'message_ok'								=>	'OK',
 	'message_no_results'				=>	'No results',
-	'message_hi'								=>	'Hi. This is the MDDA Wireless Sensor Network API v1.0'
+	'message_hi'								=>	'Hi, there. This is the MDDA Wireless Sensor Network API v1.0'
 );
